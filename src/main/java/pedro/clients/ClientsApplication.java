35af0624a742f557abcc0856c1a3413a876318be
Pedro Augusto <pedroaugustodupas@hotmail.com>
2023-11-09 16:01:52 -0300

    Test H2

--- src/main/java/pedro/clients/ClientsApplication.java
+++ src/main/java/pedro/clients/ClientsApplication.java
@@ -1,10 +1,28 @@
 package pedro.clients;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import pedro.clients.model.repository.ClientRepository;
+import pedro.clients.model.entity.Client;
 
 @SpringBootApplication
 public class ClientsApplication {
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClientRepository repository) {
+    return args -> {
+        LocalDate dataCadastro = LocalDate.now(); // Obtém a data atual
+        Client client = Client.builder().cpf("000000").nome("Pedro").dataCadastro(dataCadastro).build();
+        repository.save(client);
+    };
+}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsApplication.class, args);
--- src/main/java/pedro/clients/model/repository/ClientRepository.java
+++ src/main/java/pedro/clients/model/repository/ClientRepository.java
@@ -1,5 +1,8 @@
 package pedro.clients.model.repository;
 
-public interface ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pedro.clients.model.entity.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
     
 }
